37757ee89e6639f32e5c21cd9ac7d3e59af0dd40
Mateus Francisco Krinski <mateus.krinski10@gmail.com>
2024-05-14 22:07:43 -0300

    Setup inicial do projeto

--- src/main.py
+++ src/main.py
@@ -4,8 +4,13 @@ genai.configure(api_key="AIzaSyB9LTQSR0DgOUXRhSwmymTV0gj1CRJDr2E")
 
 model = genai.GenerativeModel("gemini-pro")
 
-response = model.generate_content("Cite animais azuis", stream=True)
+# TODO Fazer a integração de temas para busca de assuntos
+
+response = model.generate_content("Diga 1 tema do mundo animal, com no máximo 40 caracteres", stream=True)
 
 for chunk in response:
     print(chunk.text)
-    print("_"*80)
+
+# TODO Associar a geração de pubicações no X com os assuntos gerados com base no tema
+# TODO Salvar os temas em banco de dados com as datas
+# TODO Fazer a junção da API do X no código e integrar funções
